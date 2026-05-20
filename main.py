0b671054b84119bbc51f3e7654de1f13a3325abf
christiansotofajardo-cpu <christiansotofajardo@gmail.com>
2026-05-20 10:40:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2871,7 +2871,7 @@ def ocr_home():
   box.innerHTML = "";
 
   for (let i = 1; i <= n; i++) {
-
+}
     box.innerHTML +=
       "<label>Página " + i + "</label>" +
       "<input name='image_files' type='file' accept='image/png,image/jpeg,image/jpg,image/webp,image/tiff,image/bmp' required><br><br>";
